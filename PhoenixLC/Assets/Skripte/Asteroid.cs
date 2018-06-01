d054312f2509abafc0292fad6ee72ec6c0696a59
bboyAutistic <35199206+bboyAutistic@users.noreply.github.com>
2018-06-02 00:56:49 +0200

    Enemy
    note:
    asteroidima i enemyu fali explosion skripta(poziva onaj particle effect kada ih puknemo)
    enemy laser sam maknuo treba ga dodati

---
+++ PhoenixLC/Assets/Skripte/Asteroid.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+	[SerializeField]
+	float minScale = 5f;
+	[SerializeField]
+	float maxScale = 15f;
+
+	[SerializeField]
+	float rotationoffset = 50f;
+
+
+
+
+	Transform myT;
+
+	Vector3 randomRotation;
+
+	void Awake()
+	{
+		myT = transform;
+	}
+
+	void Start()
+	{
+		//random size
+		Vector3 scale= Vector3.one;
+		scale.x = Random.Range(minScale, maxScale);
+		scale.y = Random.Range(minScale, maxScale);
+		scale.z = Random.Range(minScale, maxScale);
+
+
+
+		myT.localScale = scale;
+
+		//random rotation
+		randomRotation.x = Random.Range(-rotationoffset, rotationoffset);
+		randomRotation.y = Random.Range(-rotationoffset, rotationoffset);
+		randomRotation.z = Random.Range(-rotationoffset, rotationoffset);
+
+	}
+
+	void Update () {
+		myT.Rotate(randomRotation * Time.deltaTime);
+	}
+
+
+
+}
---
+++ PhoenixLC/Assets/Skripte/PlayerHealth.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public float maxHealth = 100f;
+	public float maxShield = 100f;
+	public float shieldRegenTime = 2f;
+	public float shieldRegenAmount = 10f;
+	public GameObject deathExplosion;
+
+	GameObject shield;
+	[SerializeField]
+	GameObject gameOverScreen;
+
+	float currentHealth;
+	float currentShield;
+	bool isDead = false;
+	HealthUI healthUI;
+
+	float shieldBeforeHit;
+
+	void Awake(){
+		healthUI = GameObject.Find ("Health&Shield").GetComponent<HealthUI> ();
+		shield = GameObject.Find("Shield");
+		//izbacuje neki error ali radi
+		//shield = transform.Find("Shield").gameObject;
+	}
+
+	void Start () {
+		currentHealth = maxHealth;
+		currentShield = maxShield;
+
+		shield.SetActive(false);
+		InvokeRepeating ("RegenerateShield", shieldRegenTime, shieldRegenTime);
+	}
+
+	void RegenerateShield(){
+
+		if (currentShield < maxShield) {
+			currentShield += shieldRegenAmount;
+			healthUI.UpdateShieldBar (currentShield / maxShield);
+		}
+		if (currentShield > maxShield) {
+			currentShield = maxShield;
+			healthUI.UpdateShieldBar (currentShield / maxShield);
+		}
+
+	}
+
+	public void TakeDamage(float dmg){
+
+		CancelInvoke ("RegenerateShield");
+		InvokeRepeating ("RegenerateShield", shieldRegenTime, shieldRegenTime);
+
+		shieldBeforeHit = currentShield;
+
+		if (currentShield > 0) {
+			currentShield -= dmg;
+
+			shield.SetActive(true);
+			Invoke("DeactivateShield", 1f);
+
+
+			if (currentShield < 0) {
+				currentShield = 0;
+				dmg -= shieldBeforeHit;
+				currentHealth -= dmg;
+				healthUI.UpdateHealthBar (currentHealth / maxHealth);
+
+			}
+
+			healthUI.UpdateShieldBar (currentShield / maxShield);
+
+		} else {
+			currentHealth -= dmg;
+			if (currentHealth < 0)
+				currentHealth = 0;
+
+			healthUI.UpdateHealthBar (currentHealth / maxHealth);
+		}
+
+		if (currentHealth <= 0 && !isDead) {
+			Death ();
+		}
+	}
+
+	void Death(){
+
+		isDead = true;
+		CancelInvoke ();
+		Instantiate (deathExplosion, transform.position, transform.rotation);
+		this.gameObject.SetActive (false);
+
+		Invoke ("GameOver", 2f);
+
+		//Debug.Log("WE ARE AT 0 HEALTH");
+
+	}
+
+	void DeactivateShield()
+	{
+		shield.SetActive(false);
+	}
+
+	public float getShield(){
+		return currentShield;
+	}
+
+	void GameOver (){
+		gameOverScreen.SetActive (true);
+		Cursor.visible = true;
+		Cursor.lockState = CursorLockMode.None;
+	}
+}
