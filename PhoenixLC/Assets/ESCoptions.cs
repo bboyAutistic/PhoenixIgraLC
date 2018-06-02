79656021a532634853e524550b99bc33736acdbd
bboyAutistic <35199206+bboyAutistic@users.noreply.github.com>
2018-06-03 01:32:28 +0200

    BirdShip has arrived
    u današnje vrijeme ženke u sezoni parenja vole pokazivati svoje krasne atribude zvane lijevi i desni guz
    e pa mi čemo uskočiti u desetu i otvoriti sezonu parenja letećih svemirskih skoro pica(ovaj ptica)
    od danas letivo guzom prema POBJEDI!!!

---
+++ PhoenixLC/Assets/ESCoptions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ESCoptions : MonoBehaviour {
+
+    public GameObject pauseMenu;
+	
+	void Start () {
+        Cursor.visible = false;
+    }
+	
+	
+	void Update () {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+       
+	}
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Cursor.visible = false;
+        Time.timeScale = 1;
+    }
+}
