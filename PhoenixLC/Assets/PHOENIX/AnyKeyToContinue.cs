9f8780d53f699c1185e386f06cc6b514e0169d28
bboyAutistic <35199206+bboyAutistic@users.noreply.github.com>
2018-06-05 19:04:01 +0200

    Dodan pocetni screen i main menu ima animacije
    dodao precool poceni ekran kada krene igra
    gdje je ime igre napisano pomocu modela brodova(kao i u original igri)
    i stavio sam da nas brodek proletava ispred(jej imamo i animacije u igri hehehe)

---
+++ PhoenixLC/Assets/PHOENIX/AnyKeyToContinue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class AnyKeyToContinue : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+
+    }
+}
